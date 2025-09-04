26f24c9b8b59ea27ffb488aa075142a06d3d1e26
OsamaBin <20255103252@cse.bubt.edu.bd>
2025-09-04 09:43:14 +0600

    Refactor comments in main.cpp to provide a clearer overview of the finance management system features

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 //pending work
+//this is a finance management system
+//features to be added
+//1. add income
+//2. add expense
+//3. view balance
+//4. view transaction history
+//5. set budget
+//6. view budget status
+//7. generate reports
+//8. export data
+//9. import data
+//10. user authentication
+//11. multi-user support
 int main(){
     cout << "Manage Finance yooo boy"<<endl;
 
